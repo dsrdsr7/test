71154098e6e02609e1820d3a08d0a11f441ede3f
Mykhail <dsrdsr7@gmail.com>
2023-04-01 19:09:04 +0300

    first commit

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 
 const express = require("express");
 const app = express();
-const { Storage } = require('@google-cloud/storage');
+/*const { Storage } = require('@google-cloud/storage');
 const storage = new Storage({
     projectId: 'dogwood-garden-382315',
     keyFilename: 'C:\\Users\\User\\Desktop\\кладовка)\\dogwood-garden-382315-f58b3243e2e9.json'
-});
+});*/
 
-const bucketName = 'nure_bucket';
+/*const bucketName = 'nure_bucket';
 const bucket = storage.bucket(bucketName);
 
 const fileName = '1_image.jpg';
@@ -16,9 +16,9 @@ const file = bucket.file(fileName);
 const config = {
     action: 'read',
     expires: Date.now() + 60 * 1000 // дата истечения ссылки
-};
+};*/
 
-/*const sql = require('mssql');
+const sql = require('mssql');
 
 const config = {
     user: "dsrdsr7",
@@ -29,9 +29,9 @@ const config = {
         encrypt: true,
         trustServerCertificate: false
     },
-};*/
+};
 
-app.get("/", function(request, response){
+/*app.get("/", function(request, response){
     //const pool = new sql.ConnectionPool(config);
     file.getSignedUrl(config, function (err, url) {
         if (err) {
@@ -39,6 +39,21 @@ app.get("/", function(request, response){
             return;
         }
         response.send(`<img src="${url}" />`); // передача ссылки в тег img
+    });
+});*/
+app.get("/", function(request, response){
+    const pool = new sql.ConnectionPool(config);
+    pool.connect().then(() => {
+        const request = new sql.Request(pool);
+        request.query("SELECT * FROM MyTable").then((result) => {
+            response.send(result);
+            pool.close();
+        }).catch((err) => {
+            console.error(err);
+            pool.close();
+        });
+    }).catch((err) => {
+        console.error(err);
     });
 });
 
